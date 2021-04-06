dd9c38f88b083e4a3679329315d614f8079ebe75
= <=>
2021-04-06 18:53:48 -0400

    Re-commit

---
+++ Trees/Binary Tree Right Side View.py
@@ -0,0 +1,23 @@
+"""
+199. Binary Tree Right Side View
+Link to problem: https://leetcode.com/problems/binary-tree-right-side-view/
+
+Problem Description:
+Given the root of a binary tree, imagine yourself standing on the right side of it,
+return the values of the nodes you can see ordered from top to bottom.
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def right_side_view(self, root):
+        pass
+
+
+solution = Solution()
+tree = [1, 2, 3, None, 5, None, 4]
